8e7408ae640d151518b1f9cc3d830ebbae2c0a46
Kyle Harrison <kyle.harrison@prodigyfinance.com>
2021-08-14 18:34:44 +0200

    Add test for reading csv

--- tests/test_dump_processing.py
+++ tests/test_dump_processing.py
@@ -8,3 +8,11 @@ class TestDumpProcessing:
             file_dirs="tests/data/*.csv"
         )
         assert file_path == ["tests/data/test_league_currency.csv"]
+
+    def test_read_csv(self):
+        file_path = dump_processing.get_currency_file_paths(
+            file_dirs="tests/data/*.csv"
+        )
+        df = dump_processing.extract_df(file_path[0])
+        assert all(df.columns == ["league", "date", "currency", "value"])
+        assert df.shape == (5, 4)
