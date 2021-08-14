cd51299302464f3dd5d5bef35bfb1e6a4cc958fb
Kyle Harrison <kyle.harrison@prodigyfinance.com>
2021-08-14 18:14:38 +0200

    Add test for finding csv files

--- tests/test_dump_processing.py
+++ tests/test_dump_processing.py
@@ -1,8 +1,10 @@
 import pandas as pd
-import poepp
+from poepp.data_processing import dump_processing
 
 
 class TestDumpProcessing:
     def test_get_csv(self):
-        x = 0
-        assert x == 0
+        file_path = dump_processing.get_currency_file_paths(
+            file_dirs="tests/data/*.csv"
+        )
+        assert file_path == ["tests/data/test_league_currency.csv"]
