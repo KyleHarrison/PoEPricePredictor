b21ea82b5639fa693eca25cd69304a25eb12e5a1
Kyle Harrison <kyle.harrison@prodigyfinance.com>
2021-09-15 08:57:11 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class CleanCommand(Command):
 
 setup(
     name="poepp",
-    version="0.1.1",
+    version="0.1.2",
     author="Kyle Harrison",
     author_email="kyleharrison94@hotmail.com",
     packages=find_packages(),
